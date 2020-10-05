78a48132e9aa1c9f9a07c7f5e508067b1d790676
Geetansh Jindal <gj.address2001@gmail.com>
2020-10-05 06:58:20 +0000

    Added more configurations for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Used for a default title
 APP_NAME = 'ChucksList'   # Add
@@ -141,6 +142,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
 
